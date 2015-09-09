dbded22db33974c9ffaf16a358577e8600350bb1
kate <kate.lomax@gmail.com>
2015-09-09 13:06:35 +0100

    More base theming

--- client/templates/myCollection.js
+++ client/templates/myCollection.js
@@ -1,6 +1,9 @@
 Template.myCollection.helpers({
   annotations: function(){
     return Annotations.find({itemId: this._id});
+  },
+  myFavorites: function(){
+    return Favorites.find();
   }
 
 })
