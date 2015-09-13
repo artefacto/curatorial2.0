111c2785b87e3852649fa5828f3c41b2bb08e68a
kate <kate.lomax@gmail.com>
2015-09-13 13:35:10 +0100

    Users can delete annotations

--- client/templates/annotation.js
+++ client/templates/annotation.js
@@ -8,7 +8,25 @@ Template.annotation.helpers({
   publicAnnotations: function(){
     return Annotations.find({publish: true})
   }
+
 })
+
+
+Template.annotation.events({
+      "click .toggle-checked": function () {
+        // Set the publish property to the opposite of its current value
+        Annotations.update(this._id, {
+          $set: {checked: ! this.checked}
+        });
+      },
+      'click .delete': function (e) {
+        e.preventDefault();
+        if (confirm("Delete this annotation forever?")) {
+
+          Annotations.remove(this._id);
+        }
+      }
+    });
 
 
 AutoForm.hooks({
