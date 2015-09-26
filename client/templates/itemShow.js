90535f65061a238e05321895bb307603332d768e
kate <kate.lomax@gmail.com>
2015-09-26 15:07:27 +0100

    Add annotation status and public annotation view

--- client/templates/itemShow.js
+++ client/templates/itemShow.js
@@ -1,11 +1,15 @@
 
 
 Meteor.subscribe("itemAnnotations");
-
+//Meteor.subscribe("publishedAnnotations");
 Template.itemShow.helpers({
   annotations: function(){
     return Annotations.find({itemId: this._id});
   },
+   sharedAnnotations: function(){
+       return Annotations.find({itemId: this._id, publish: true});
+   },
+
   currentPage: function(){
         if (Iron.Location.get().rootUrl.length) {
           return Iron.Location.get().href
